59d33cdba00ab9dc18d2ab155444fe9331cdbf41
최민규 <yuming_cm4lx@naver.com>
2024-07-22 16:36:02 +0900

    chore: 메인 페이지 로그인/비로그인 컴포넌트 구분함

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,7 +1,11 @@
+import GuestContents from "./components/GuestContents";
+import MemberContents from "./components/MemberContents";
+
 export default function Home() {
-    return (
-        <>
-            <h1>홈페이지임</h1>
-        </>
-    );
+  return (
+    <>
+      <h1>홈페이지임</h1>
+      {/* 로그인 안되어 있으면 GuestContents, 로그인 되어 있으면 MemberContents 출력 */}
+    </>
+  );
 }
---
+++ frontend/src/routes/components/MemberContents.jsx
@@ -0,0 +1,7 @@
+export default function MemberContents() {
+  return (
+    <>
+      <h1>회원 전용 페이지임</h1>
+    </>
+  );
+}
