96ba07f0bb7fd58d11c6c6a6e71fa19ff5851f39
unknown <enduf768640@gmail.com>
2024-07-22 09:37:14 +0900

    Initial Commit (Backend)

---
+++ ssafyro/src/test/java/com/ssafy/ssafyro/SsafyroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafyro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafyroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
