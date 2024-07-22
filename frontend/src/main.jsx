c8372f8bd6aa04925c42527c9eabe016fc0a04b8
최민규 <yuming_cm4lx@naver.com>
2024-07-22 10:33:18 +0900

    feat: 2차 면접 가이드 관련 컴포넌트 및 footer 컴포넌트 추가

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,17 +11,42 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import routes
 import Home from "./routes/Home.jsx";
+import GuidePersonality from "./routes/second/guide/GuidePersonality.jsx";
+import GuidePT from "./routes/second/guide/GuidePT.jsx";
+import GuideIT from "./routes/second/guide/GuideIT.jsx";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-    },
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/second",
+    children: [
+      {
+        path: "guide",
+        children: [
+          {
+            path: "personality",
+            element: <GuidePersonality />,
+          },
+          {
+            path: "pt",
+            element: <GuidePT />,
+          },
+          {
+            path: "it",
+            element: <GuideIT />,
+          },
+        ],
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <Navbar />
-        <RouterProvider router={router} />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Navbar />
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
