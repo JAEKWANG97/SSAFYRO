83e327ee3036a2dd5a473bf484735966b5a35e3f
박지훈 <pjh2996@naver.com>
2024-07-24 00:26:25 +0900

    test: 면접 질문, 답변, 표정, 발음 점수 저장 테스트

---
+++ backend/src/test/java/com/ssafy/ssafyro/domain/Interview/InterviewingRepositoryTest.java
@@ -0,0 +1,107 @@
+package com.ssafy.ssafyro.domain.Interview;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional
+class InterviewingRepositoryTest {
+
+    @Autowired
+    private InterviewingRepository interviewingRepository;
+
+    @DisplayName("면접 질문 답변 후 표정, 억양, 질문, 답변에 대한 내용을 Redis 에 저장한다.")
+    @Test
+    void saveQuestionResult() {
+        //given
+        double personOrderNumber = 1.0;
+        String userId = "user";
+        Interview interviewing = createInterviewing(personOrderNumber, userId);
+
+        //when
+        Interview saved = interviewingRepository.save(interviewing);
+
+        //then
+        assertThat(saved)
+                .extracting("roomId", "userId", "question", "answer",
+                        "pronunciationScore", "happy", "disgust", "sad",
+                        "surprise", "fear", "angry", "neutral")
+                .contains("room", userId, "question" + personOrderNumber,
+                        "answer" + personOrderNumber,
+                        (int) personOrderNumber, personOrderNumber, personOrderNumber, personOrderNumber,
+                        personOrderNumber, personOrderNumber, personOrderNumber, personOrderNumber);
+    }
+
+    @DisplayName("면접 질문 답변 저장 시, 한 사람에게 여러 질문이 저장될 수 있다.")
+    @Test
+    void saveQuestionResult2() {
+        //given
+        double personOrderNumber = 1.0;
+        String userId = "user";
+        Interview interviewing1 = createInterviewing(personOrderNumber, userId);
+        Interview interviewing2 = createInterviewing(++personOrderNumber, userId);
+        Interview interviewing3 = createInterviewing(++personOrderNumber, userId);
+
+        //when
+        Iterable<Interview> result = interviewingRepository.saveAll(
+                List.of(interviewing1, interviewing2, interviewing3));
+
+        //then
+        assertThat(result).hasSize(3)
+                .extracting("question", "answer", "happy")
+                .containsExactlyInAnyOrder(
+                        tuple("question1.0", "answer1.0", 1.0),
+                        tuple("question2.0", "answer2.0", 2.0),
+                        tuple("question3.0", "answer3.0", 3.0)
+                );
+    }
+
+    @DisplayName("면접 질문 답변 저장 시, 여러 사람에게 여러 질문이 저장될 수 있다.")
+    @Test
+    void saveQuestionResult3() {
+        //given
+        double personOrderNumber = 1.0;
+        Interview interviewing1 = createInterviewing(personOrderNumber, "user1");
+        Interview interviewing2 = createInterviewing(++personOrderNumber, "user1");
+        Interview interviewing3 = createInterviewing(++personOrderNumber, "user2");
+
+        //when
+        Iterable<Interview> result = interviewingRepository.saveAll(
+                List.of(interviewing1, interviewing2, interviewing3));
+
+        //then
+        assertThat(result).hasSize(3)
+                .extracting("userId", "question", "answer", "happy")
+                .containsExactlyInAnyOrder(
+                        tuple("user1", "question1.0", "answer1.0", 1.0),
+                        tuple("user1", "question2.0", "answer2.0", 2.0),
+                        tuple("user2", "question3.0", "answer3.0", 3.0)
+                );
+    }
+
+    private Interview createInterviewing(double personOrderNumber, String userId) {
+        return Interview.builder()
+                .roomId("room")
+                .userId(userId)
+                .question("question" + personOrderNumber)
+                .answer("answer" + personOrderNumber)
+                .pronunciationScore((int) personOrderNumber)
+                .happy(personOrderNumber)
+                .disgust(personOrderNumber)
+                .sad(personOrderNumber)
+                .surprise(personOrderNumber)
+                .fear(personOrderNumber)
+                .angry(personOrderNumber)
+                .neutral(personOrderNumber)
+                .build();
+    }
+}
