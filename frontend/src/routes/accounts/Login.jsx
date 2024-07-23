49b1a856abf7001b2002fe5e94dd0e4bd097e03c
최민규 <yuming_cm4lx@naver.com>
2024-07-23 11:15:32 +0900

    feat: 로그인 버튼 Login w/ Google로 바꿈

--- frontend/src/routes/accounts/Login.jsx
+++ frontend/src/routes/accounts/Login.jsx
@@ -3,8 +3,14 @@ import LoginForm from "./components/LoginForm";
 export default function Login() {
   return (
     <>
-      <div>로그인 화면임</div>
-      <LoginForm />
+      <div className="h-dvh flex justify-center items-center">
+        <div className="h-[400px] w-[400px] rounded-lg shadow-xl flex flex-col justify-start items-center">
+          <div className="text-4xl font-extrabold text-[#90CCF0] my-24">
+            SSAFYRO
+          </div>
+          <LoginForm />
+        </div>
+      </div>
     </>
   );
 }
