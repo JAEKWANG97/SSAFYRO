c8e0fe3433a631b0b9b30f432b25f94d2a7bb3b1
박지훈 <pjh2996@naver.com>
2024-07-24 11:26:57 +0900

    refactor: redis 환경변수 생성자 방식 사용

--- backend/src/main/java/com/ssafy/ssafyro/config/RedisConfig.java
+++ backend/src/main/java/com/ssafy/ssafyro/config/RedisConfig.java
@@ -1,28 +1,19 @@
 package com.ssafy.ssafyro.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
-/**
- * Redis 환경 설정
- */
-@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(RedisProperties.class)
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
+    private final RedisProperties properties;
 
-    @Value("${spring.data.redis.port}")
-    private int port;
-
-    /**
-     * Redis 와의 연결을 위한 'Connection'을 생성합니다.
-     */
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        return new LettuceConnectionFactory(properties.getHost(), properties.getPort());
     }
 }
