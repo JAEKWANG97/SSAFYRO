b5e638279da283d05b2b4b00b894f26e2ab067dc
박지훈 <pjh2996@naver.com>
2024-07-24 11:11:00 +0900

    rename: 면접 질문에 대한 결과값을 나타내는 클래스 이름 변경

--- backend/src/main/java/com/ssafy/ssafyro/api/service/interview/InterviewService.java
+++ backend/src/main/java/com/ssafy/ssafyro/api/service/interview/InterviewService.java
@@ -1,11 +1,11 @@
 package com.ssafy.ssafyro.api.service.interview;
 
 import com.ssafy.ssafyro.api.controller.interview.request.StartRequest;
-import com.ssafy.ssafyro.api.service.interview.request.QuestionResultServiceRequest;
+import com.ssafy.ssafyro.api.service.interview.request.QnAResultServiceRequest;
 import com.ssafy.ssafyro.api.service.interview.response.ArticleResponse;
 import com.ssafy.ssafyro.api.service.interview.response.StartResponse;
 import com.ssafy.ssafyro.domain.Interview.Interview;
-import com.ssafy.ssafyro.domain.Interview.InterviewingRepository;
+import com.ssafy.ssafyro.domain.Interview.InterviewRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class InterviewService {
 
-    private final InterviewingRepository interviewingRepository;
+    private final InterviewRepository interviewRepository;
 
     public StartResponse startInterview(StartRequest startRequest) {
         return new StartResponse("모집완료");
@@ -24,9 +24,9 @@ public class InterviewService {
         return new ArticleResponse("", "", List.of());
     }
 
-    public void saveQuestionResult(QuestionResultServiceRequest request) {
-        Interview interviewing = request.toEntity();
-        interviewingRepository.save(interviewing);
+    public void saveQnAResult(QnAResultServiceRequest request) {
+        Interview interview = request.toEntity();
+        interviewRepository.save(interview);
     }
 
 }
--- backend/src/main/java/com/ssafy/ssafyro/domain/Interview/InterviewRepository.java
+++ backend/src/main/java/com/ssafy/ssafyro/domain/Interview/InterviewRepository.java
@@ -2,6 +2,6 @@ package com.ssafy.ssafyro.domain.Interview;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface InterviewingRepository extends CrudRepository<Interview, String> {
+public interface InterviewRepository extends CrudRepository<Interview, String> {
 //    Iterable<Interview> findByRoomIdAndUserId(String roomId, String userId);
 }
