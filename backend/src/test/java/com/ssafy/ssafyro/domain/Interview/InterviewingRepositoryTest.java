5e1931aaafd74fbc9eea112aa9771e5b4c52e4ab
박지훈 <pjh2996@naver.com>
2024-07-24 10:55:40 +0900

    refactor: 테스트 코드 환경 통합화

--- backend/src/test/java/com/ssafy/ssafyro/domain/Interview/InterviewingRepositoryTest.java
+++ backend/src/test/java/com/ssafy/ssafyro/domain/Interview/InterviewingRepositoryTest.java
@@ -3,18 +3,15 @@ package com.ssafy.ssafyro.domain.Interview;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
+import com.ssafy.ssafyro.IntegrationTestSupport;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-@ActiveProfiles("test")
-@SpringBootTest
 @Transactional
-class InterviewingRepositoryTest {
+class InterviewingRepositoryTest extends IntegrationTestSupport {
 
     @Autowired
     private InterviewingRepository interviewingRepository;
