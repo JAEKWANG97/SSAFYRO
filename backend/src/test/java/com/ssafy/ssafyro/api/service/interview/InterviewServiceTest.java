67fa67bf4e598fd3557902addd5ec3414197d606
박지훈 <pjh2996@naver.com>
2024-07-25 11:50:45 +0900

    test: 면접 질문 결과 저장 서비스 로직 테스트

--- backend/src/test/java/com/ssafy/ssafyro/api/service/interview/InterviewServiceTest.java
+++ backend/src/test/java/com/ssafy/ssafyro/api/service/interview/InterviewServiceTest.java
@@ -9,12 +9,15 @@ import com.ssafy.ssafyro.api.controller.interview.request.QnAResultRequest;
 import com.ssafy.ssafyro.api.service.interview.request.QnAResultServiceRequest;
 import com.ssafy.ssafyro.domain.interview.InterviewRedis;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 
 class InterviewServiceTest extends IntegrationTestSupport {
+
+    private final List<String> KEYS = List.of("interview:userId1", "interview:userId2", "interview:userId3");
 
     @Autowired
     private InterviewService interviewService;
@@ -25,11 +28,17 @@ class InterviewServiceTest extends IntegrationTestSupport {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @AfterEach
+    void tearDown() {
+        redisTemplate.delete(KEYS);
+    }
+
     @DisplayName("면접 답변에 대한 질문, 답변, 표정, 발음에 대한 점수를 Json 형식으로 변환 후 List 에 저장한다.")
     @Test
     void saveQnAResult() throws JsonProcessingException {
         //given
-        String userId = "userId1";
+        String key = KEYS.get(0);
+        String userId = key.split(":")[1];
         double orderNumber = 1.0;
         QnAResultRequest request = createQnAResultRequest(orderNumber, userId);
         QnAResultServiceRequest serviceRequest = request.toServiceRequest();
@@ -47,13 +56,38 @@ class InterviewServiceTest extends IntegrationTestSupport {
                 .angry(orderNumber)
                 .neutral(orderNumber)
                 .build());
+
         //when
         interviewService.saveQnAResult(serviceRequest);
 
         //then
-        List<Object> savedValues = redisTemplate.opsForList().range(userId, 0, -1);
+        List<Object> savedValues = redisTemplate.opsForList().range(key, 0, -1);
         assertThat(savedValues).hasSize(1);
-//        assertThat(savedValues).contains(expectedJson);
+        assertThat(savedValues).contains(expectedJson);
+    }
+
+    @DisplayName("면접 시 한 사람에게 여러 질문을 저장할 수 있다.")
+    @Test
+    void saveQnAResult2() throws JsonProcessingException {
+        //given
+        String key = KEYS.get(0);
+        String userId = key.split(":")[1];
+        double orderNumber = 1.0;
+        QnAResultRequest request1 = createQnAResultRequest(orderNumber, userId);
+        QnAResultRequest request2 = createQnAResultRequest(++orderNumber, userId);
+        QnAResultRequest request3 = createQnAResultRequest(++orderNumber, userId);
+        QnAResultServiceRequest serviceRequest1 = request1.toServiceRequest();
+        QnAResultServiceRequest serviceRequest2 = request2.toServiceRequest();
+        QnAResultServiceRequest serviceRequest3 = request3.toServiceRequest();
+
+        //when
+        interviewService.saveQnAResult(serviceRequest1);
+        interviewService.saveQnAResult(serviceRequest2);
+        interviewService.saveQnAResult(serviceRequest3);
+
+        //then
+//        List<Object> savedValues = redisTemplate.opsForList().range(key, 0, -1);
+//        assertThat(savedValues).hasSize(1);
     }
 
     private QnAResultRequest createQnAResultRequest(double orderNumber, String userId) {
