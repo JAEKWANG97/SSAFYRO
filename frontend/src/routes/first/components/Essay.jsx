8e9f50355c3376caa15fd055fe773dc7f5646960
고도연 <focus0007@naver.com>
2024-07-25 15:44:26 +0900

    feat: Ismajor 컴포넌트

--- frontend/src/routes/first/components/Essay.jsx
+++ frontend/src/routes/first/components/Essay.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react';
-import Button from '../../../components/Button';
+import useFirstStore from '../../../stores/FirstStore';
+import Ismajor from '../../../components/Ismajor';
 
 export default function Essay() {
-  const [selected, setSelected] = useState('major');
-  const [showCorrection, setShowCorrection] = useState(false);
-
-  const handleSelect = (type) => {
-    setSelected(type);
-    setShowCorrection(false); // 새로운 타입을 선택할 때 AI 첨삭 상태 초기화
-  };
-
+  const selected = useFirstStore((state) => state.selected)
+  const showCorrection = useFirstStore((state) => state.showCorrection)
+  const setShowCorrection = useFirstStore((state) => state.setShowCorrection)
   const handleAiCorrection = () => {
     setShowCorrection(true);
   };
@@ -18,29 +13,7 @@ export default function Essay() {
     <>
       <div className='flex border-b pb-4 items-center'>
         <p className="text-3xl font-bold pr-4">에세이</p>
-        <div className="inline-flex rounded-md shadow-sm" role="group">
-          <button 
-            type="button" 
-            className={`px-4 py-2 text-sm font-bold bg-white border border-gray-200 rounded-s-lg ${
-              selected === 'major' 
-                ? 'font-bold text-[#90CCF0] border-[#90CCF0] focus:ring-[#90CCF0] focus:border-[#90CCF0]'
-                : 'font-bold text-gray-900 hover:text-[#90CCF0] focus:z-10 focus:ring-2 focus:ring-[#90CCF0] focus:text-[#90CCF0]'
-            }`}
-            onClick={() => handleSelect('major')}>
-            전공자
-          </button>
-
-          <button 
-            type="button" 
-            className={`px-4 py-2 text-sm font-bold bg-white border border-gray-200 rounded-e-lg ${
-              selected === 'nonMajor' 
-                ? 'text-[#90CCF0] border-[#90CCF0] focus:ring-[#90CCF0] focus:border-[#90CCF0]'
-                : 'text-gray-900 hover:text-[#90CCF0] focus:z-10 focus:ring-2 focus:ring-[#90CCF0] focus:text-[#90CCF0]'
-            }`}
-            onClick={() => handleSelect('nonMajor')}>
-            비전공자
-          </button>
-        </div>
+        <Ismajor/>
         <button
           type="button"
           className="ml-auto text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
@@ -86,6 +59,8 @@ export default function Essay() {
           >
             저장
           </button>
+
+         
         </div>
       </div>
     </>
--- frontend/src/routes/first/components/Test.jsx
+++ frontend/src/routes/first/components/Test.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react';
+import useFirstStore from '../../../stores/FirstStore';
+import Ismajor from '../../../components/Ismajor';
+
 
 export default function Test() {
-  const [selected, setSelected] = useState('major');
-  const [showCorrection, setShowCorrection] = useState(false);
-
-  const handleSelect = (type) => {
-    setSelected(type);
-    setShowCorrection(false); // 새로운 타입을 선택할 때 AI 첨삭 상태 초기화
-  };
+  const selected = useFirstStore((state) => state.selected)
+  
 
   const cards = [
     {
@@ -46,29 +43,7 @@ export default function Test() {
     <>
       <div className='flex border-b pb-4 items-center'>
         <p className="text-3xl font-bold pr-4">SW 적성진단</p>
-        <div className="inline-flex rounded-md shadow-sm" role="group">
-          <button 
-            type="button" 
-            className={`px-4 py-2 text-sm font-bold bg-white border border-gray-200 rounded-s-lg ${
-              selected === 'major' 
-                ? 'font-bold text-[#90CCF0] border-[#90CCF0] focus:ring-[#90CCF0] focus:border-[#90CCF0]'
-                : 'font-bold text-gray-900 hover:text-[#90CCF0] focus:z-10 focus:ring-2 focus:ring-[#90CCF0] focus:text-[#90CCF0]'
-            }`}
-            onClick={() => handleSelect('major')}>
-            전공자
-          </button>
-
-          <button 
-            type="button" 
-            className={`px-4 py-2 text-sm font-bold bg-white border border-gray-200 rounded-e-lg ${
-              selected === 'nonMajor' 
-                ? 'text-[#90CCF0] border-[#90CCF0] focus:ring-[#90CCF0] focus:border-[#90CCF0]'
-                : 'text-gray-900 hover:text-[#90CCF0] focus:z-10 focus:ring-2 focus:ring-[#90CCF0] focus:text-[#90CCF0]'
-            }`}
-            onClick={() => handleSelect('nonMajor')}>
-            비전공자
-          </button>
-        </div>
+        <Ismajor/>
       </div>
       
 
