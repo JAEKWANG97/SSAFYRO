04945c08c6260171cb3205569045571f450ff9e8
김두열 <enduf768640@gmail.com>
2024-07-26 13:24:09 +0900

    refactor: openvidu token 발급 api key 추가

--- backend/src/main/java/com/ssafy/ssafyro/api/controller/openvidu/OpenViduTokenFactory.java
+++ backend/src/main/java/com/ssafy/ssafyro/api/controller/openvidu/OpenViduTokenFactory.java
@@ -3,13 +3,20 @@ package com.ssafy.ssafyro.api.controller.openvidu;
 import io.livekit.server.AccessToken;
 import io.livekit.server.RoomJoin;
 import io.livekit.server.RoomName;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OpenViduTokenFactory {
 
+    @Value("${livekit.api.key}")
+    private String LIVEKIT_API_KEY;
+
+    @Value("${livekit.api.secret}")
+    private String LIVEKIT_API_SECRET;
+
     public String createTokenBy(String roomName, String participantName) {
-        AccessToken token = new AccessToken("LIVEKIT_API_KEY", "LIVEKIT_API_SECRET");
+        AccessToken token = new AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
         token.setName(participantName);
         token.setIdentity(participantName);
         token.addGrants(new RoomJoin(true), new RoomName(roomName));
