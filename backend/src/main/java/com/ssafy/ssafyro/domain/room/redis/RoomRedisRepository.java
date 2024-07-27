8be70853234a591734acb356e522ab8955c6d756
김두열 <enduf768640@gmail.com>
2024-07-27 17:15:14 +0900

    fix: RoomRedisRepository save 키 설정 오류 수정

--- backend/src/main/java/com/ssafy/ssafyro/domain/room/redis/RoomRedisRepository.java
+++ backend/src/main/java/com/ssafy/ssafyro/domain/room/redis/RoomRedisRepository.java
@@ -1,7 +1,6 @@
 package com.ssafy.ssafyro.domain.room.redis;
 
 import java.util.Optional;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
@@ -15,23 +14,12 @@ public class RoomRedisRepository {
     private final RedisTemplate<String, RoomRedis> redisTemplate;
 
     public String save(RoomRedis roomRedis) {
-        String key = generateRandomKey();
-        redisTemplate.opsForValue().set(KEY_PREFIX + key, roomRedis);
+        redisTemplate.opsForValue().set(KEY_PREFIX + roomRedis.getId(), roomRedis);
 
-        return key;
-    }
-
-    public String save(String key, RoomRedis roomRedis) {
-        redisTemplate.opsForValue().set(KEY_PREFIX + key, roomRedis);
-
-        return key;
+        return roomRedis.getId();
     }
 
     public Optional<RoomRedis> findById(String key) {
         return Optional.ofNullable(redisTemplate.opsForValue().get(KEY_PREFIX + key));
-    }
-
-    private String generateRandomKey() {
-        return UUID.randomUUID().toString();
     }
 }
