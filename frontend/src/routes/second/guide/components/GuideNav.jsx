a26ebefcb3f33c72b900c3b6807972985b2afa38
고도연 <185520@jnu.ac.kr>
2024-07-28 01:23:49 +0900

    design: second 페이지 버튼 디자인 수정 완료

--- frontend/src/routes/second/guide/components/GuideNav.jsx
+++ frontend/src/routes/second/guide/components/GuideNav.jsx
@@ -5,29 +5,28 @@ export default function GuideNav() {
   const nav = useNavigate();
   const location = useLocation();
 
-  // 현재 경로와 일치하는지 확인하는 함수
   const isActive = (path) => location.pathname === path;
 
   return (
     <>
       <nav className="flex py-2 px-4">
         <Button 
-          text={'인성면접'} 
-          type={'GUIDENAV'} 
+          text="인성면접" 
+          type="GUIDENAV" 
           onClick={() => nav('/second/guide/personality')}
-          className={isActive('/second/guide/personality') ? 'bg-[#90CCF0] text-white' : ''}
+          isActive={isActive('/second/guide/personality')}
         />
         <Button 
-          text={'PT면접'} 
-          type={'GUIDENAV'} 
+          text="PT면접" 
+          type="GUIDENAV" 
           onClick={() => nav('/second/guide/pt')}
-          className={isActive('/second/guide/pt') ? 'bg-[#90CCF0] text-white' : ''}
+          isActive={isActive('/second/guide/pt')}
         />
         <Button 
-          text={'최신 IT 이슈'} 
-          type={'GUIDENAV'} 
+          text="최신 IT 이슈" 
+          type="GUIDENAV" 
           onClick={() => nav('/second/guide/it')}
-          className={isActive('/second/guide/it') ? 'bg-[#90CCF0] text-white' : ''}
+          isActive={isActive('/second/guide/it')}
         />
       </nav>
     </>
