13bffc01df98827407f0eddb72543cdf4adf1742
고도연 <185520@jnu.ac.kr>
2024-07-30 22:29:21 +0900

    feature: 면접 가이드 레이아웃 및 문구 정리

--- frontend/src/routes/second/guide/GuidePT.jsx
+++ frontend/src/routes/second/guide/GuidePT.jsx
@@ -1,34 +1,52 @@
+import React from "react";
 import SecondNav from "../components/SecondNav.jsx";
 import GuideNav from "./components/GuideNav.jsx";
-import personalityimg from './../../../../public/first/personality.png';
+import PtImg from './../../../../public/first/pt.png';
 
 export default function GuidePT() {
   return (
     <>
       <SecondNav />
       <GuideNav />
-      <div className="container mx-auto p-5 max-w-3xl bg-gray-50 rounded-lg shadow-lg mt-10">
-        <div className="image-container text-center mb-8">
-          <img src={personalityimg} className="main-image w-[400px] h-[300px] rounded-lg mx-auto" alt="personalityimg" />
+      <div className="container mx-auto p-5 max-w-3xl bg-white rounded-lg shadow-lg mt-10">
+        <div className="text-center mb-8">
+          <img src={PtImg} className="w-[400px] h-[300px] rounded-lg mx-auto" alt="PT 면접 가이드 이미지" />
         </div>
-        <div className="content px-10">
-          <h1 className="title text-3xl mt-12 mb-10 font-bold text-center text-gray-800">PT 면접 가이드</h1>
-          <div className="text text-base mb-5 text-gray-700 leading-loose">
-            <h2 className="text-2xl font-semibold mb-4">면접 진행 절차</h2>
-            <ul className="list-disc list-inside mb-6 space-y-2">
-              <li>pt 모의 면접은 싸피로에서 준비한 주제와 질문을 기반으로 진행됩니다.</li>
-              <li>모의면접 참여인원이 1인일 경우, ai 면접이 진행됩니다. 2인 이상일 경우 다대일 비대면 면접이 진행됩니다.</li>
-              <li>2인 이상일 경우 역할(면접자, 면접관)이 교대되면서 진행됩니다.</li>
-              <li>PT 발표 이후, 본인이 발표한 내용을 기반으로 AI 기반의 질의응답이 이루어집니다.</li>
+        <div className="px-8">
+          <h1 className="text-3xl mt-8 mb-8 font-bold text-center text-gray-900">PT 면접 가이드</h1>
+          <div className="text-lg px-6 mb-6 text-gray-800 leading-relaxed">
+            <ul className="list-none mb-6 space-y-2">
+              <li className="flex items-start">
+                <span className="text-blue-500 mr-2">✔️</span>
+                <span>PT 모의 면접은 싸피로에서 준비한 주제와 질문을 기반으로 진행됩니다.</span>
+              </li>
+              <li className="flex items-start">
+                <span className="text-blue-500 mr-2">✔️</span>
+                <span className="ml-2">모의면접 참여 인원이 <strong>1인</strong>일 경우, <strong>AI 면접</strong>이 진행됩니다.</span>
+              </li>
+              <li className="flex items-start">
+                <span className="text-blue-500 mr-2">✔️</span>
+                <span className="ml-2"><strong>2인 이상</strong>일 경우, <strong>다대일 비대면 면접</strong>이 진행됩니다.</span>
+              </li>
+              <li className="flex items-start">
+                <span className="text-blue-500 mr-2">✔️</span>
+                <span className="ml-2"><strong>2인 이상</strong>일 경우,  <strong>역할(면접자, 면접관)이 교대</strong>되면서 진행됩니다.</span>
+              </li>
+              <li className="flex items-start">
+                <span className="text-blue-500 mr-2">✔️</span>
+                <div>
+                  <span>PT 발표 이후, 발표한 내용을 기반으로 AI가 면접 질문을 생성하여</span>  
+                  <span className="block">질의 혹은 추천해줍니다.</span>
+                </div>
+              </li>
             </ul>
 
-            <h2 className="text-2xl font-semibold mb-4">설명서</h2>
+            <h2 className="text-2xl font-semibold mb-4">미리보기</h2>
             <ol className="list-decimal list-inside mb-6 space-y-2">
-              <li>에세이 탭에서 전공 유무에 따른 에세이 예상 질문을 확인해주세요.</li>
-              <li>주어진 질문에 맞게 에세이를 작성하고 저장해주세요. AI 첨삭도 받을 수 있어요!</li>
-              <p className="ml-5 text-sm">※ 프로필의 에세이 란에 작성한 에세이를 수정 및 업로드할 수 있습니다.</p>
-              <li>면접 연습하기 탭에서 직접 인성 면접 방을 생성하거나, 생성된 인성 면접 방에 들어가서 빠르게 면접을 시작할 수 있어요.</li>
-              <li>면접 대기실에서 인성 면접 진행 팁을 숙지한 후, 면접을 시작해주세요!</li>
+              <li>디자인 완료된 후 사진 들어감</li>
+              <li>PT 면접은 최근 IT 이슈를 파악하는 것이 중요합니다. 최근 IT 이슈 탭에서 싸피로가 선별한 다양한 IT 이슈를 공부해보세요!</li>
+              <li>면접 연습하기 탭에서 직접 PT 면접 방을 생성하거나, 생성된 PT 면접 방에 들어가서 빠르게 면접을 시작할 수 있습니다.</li>
+              <li>면접 대기실에서 PT 면접 진행 팁을 숙지한 후, 면접을 시작하세요!</li>
             </ol>
           </div>
         </div>
--- frontend/src/routes/second/guide/GuidePersonality.jsx
+++ frontend/src/routes/second/guide/GuidePersonality.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import SecondNav from "../components/SecondNav.jsx";
 import GuideNav from "./components/GuideNav.jsx";
 import personalityimg from './../../../../public/first/personality.png';
@@ -8,29 +7,47 @@ export default function GuidePersonality() {
     <>
       <SecondNav />
       <GuideNav />
-      <div className="container mx-auto p-5 max-w-3xl bg-gray-50 rounded-lg shadow-lg mt-10">
-        <div className="image-container text-center mb-8">
-          <img src={personalityimg} className="main-image w-[400px] h-[300px] rounded-lg mx-auto" alt="personalityimg" />
+      <div className="container mx-auto p-5 max-w-3xl bg-white rounded-lg shadow-lg mt-10">
+        <div className="text-center mb-8">
+          <img src={personalityimg} className="w-[400px] h-[300px] rounded-lg mx-auto" alt="인성 면접 가이드 이미지" />
         </div>
-        <div className="content px-10">
-          <h1 className="title text-3xl mt-12 mb-10 font-bold text-center text-gray-800">인성 면접 가이드</h1>
-          <div className="text text-base mb-5 text-gray-700 leading-loose">
-            <h2 className="text-2xl font-semibold mb-4">면접 진행 절차</h2>
-            <ul className="list-disc list-inside mb-6 space-y-2">
-              <li>인성 모의 면접은 에세이 기반으로 진행됩니다.</li>
-              <li>모의면접 참여인원이 1인일 경우, AI 면접이 진행됩니다.</li>
-              <li>2인 이상일 경우 다대일 비대면 면접이 진행됩니다.</li>
-              <li>2인 이상일 경우 역할(면접자, 면접관)이 교대되면서 진행됩니다.</li>
-              <li>1분 자기소개를 비롯하여, 본인이 작성한 에세이를 기반으로 AI 기반의 질의응답이 이루어집니다.</li>
-            </ul>
+        <div className="px-8">
+          <h1 className="text-3xl mt-8 mb-8 font-bold text-center text-gray-900">인성 면접 가이드</h1>
+          <div className="text-lg mb-6 px-10 text-gray-800 leading-relaxed">
+            <ol className="list-none mb-6 space-y-2">
+              <li className="flex items-center">
+                <span className="text-blue-500">✔️</span>
+                <span className="ml-2">인성 모의 면접은 <strong>에세이 기반</strong>으로 진행됩니다.</span>
+              </li>
+              <li className="flex items-center">
+                <span className="text-blue-500">✔️</span>
+                <span className="ml-2">모의면접 참여 인원이 <strong>1인</strong>일 경우, <strong>AI 면접</strong>이 진행됩니다.</span>
+              </li>
+              <li className="flex items-center">
+                <span className="text-blue-500">✔️</span>
+                <span className="ml-2"><strong>2인 이상</strong>일 경우, <strong>다대일 비대면 면접</strong>이 진행됩니다.</span>
+              </li>
+              <li className="flex items-center">
+                <span className="text-blue-500">✔️</span>
+                <span className="ml-2"><strong>2인 이상</strong>일 경우,  <strong>역할(면접자, 면접관)이 교대</strong>되면서 진행됩니다.</span>
+              </li>
+              <li className="flex items-start">
+                <span className="text-blue-500">✔️</span>
+                <div className="ml-2">
+                  <span>1분 자기소개 후, 에세이 기반으로 AI가 면접 질문을 생성하여</span>  
+                  <span className="block ml-1">질의 혹은 추천해줍니다.</span>
+                </div>
+              </li>
+            </ol>
 
-            <h2 className="text-2xl font-semibold mb-4">설명서</h2>
+            <h2 className="text-2xl font-semibold mb-4">미리보기</h2>
             <ol className="list-decimal list-inside mb-6 space-y-2">
-              <li>에세이 탭에서 전공 유무에 따른 에세이 예상 질문을 확인해주세요.</li>
-              <li>주어진 질문에 맞게 에세이를 작성하고 저장해주세요. AI 첨삭도 받을 수 있어요!</li>
-              <p className="ml-5 text-sm">※ 프로필의 에세이 란에 작성한 에세이를 수정 및 업로드할 수 있습니다.</p>
-              <li>면접 연습하기 탭에서 직접 인성 면접 방을 생성하거나, 생성된 인성 면접 방에 들어가서 빠르게 면접을 시작할 수 있어요.</li>
-              <li>면접 대기실에서 인성 면접 진행 팁을 숙지한 후, 면접을 시작해주세요!</li>
+              <li>디자인 완료된 후 사진 들어감</li>
+              <li>에세이 탭에서 전공 유무에 따른 에세이 예상 질문을 확인하세요.</li>
+              <li>주어진 질문에 맞게 에세이를 작성하고 저장하세요. AI 첨삭도 받을 수 있습니다.</li>
+              <p className="ml-5 text-sm text-gray-600">※ 프로필의 에세이 란에서 작성한 에세이를 수정 및 업로드할 수 있습니다.</p>
+              <li>면접 연습하기 탭에서 직접 인성 면접 방을 생성하거나, 생성된 인성 면접 방에 들어가서 빠르게 면접을 시작할 수 있습니다.</li>
+              <li>면접 대기실에서 면접 진행 팁을 숙지한 후, 면접을 시작하세요!</li>
             </ol>
           </div>
         </div>
