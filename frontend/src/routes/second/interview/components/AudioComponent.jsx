4b122458fd87c96cfcdb44b5ef872ab96960eedc
최민규 <yuming_cm4lx@naver.com>
2024-07-30 14:13:11 +0900

    feat: AudioComponent 초안 작성

--- frontend/src/routes/second/interview/components/AudioComponent.jsx
+++ frontend/src/routes/second/interview/components/AudioComponent.jsx
@@ -1,3 +1,18 @@
-export default function AudioComponent() {
-  return <></>;
+import { useEffect, useRef } from "react";
+import { LocalAudioTrack, RemoteAudioTrack } from "livekit-client";
+
+export default function AudioComponent({ track }) {
+  const audioElement = useRef(null);
+
+  useEffect(() => {
+    if (audioElement.current) {
+      track.attach(audioElement.current);
+    }
+
+    return () => {
+      track.detach();
+    };
+  }, [track]);
+
+  return <audio ref={audioElement} id={track.sid} />;
 }
