69c7836f053aadddaa36dc3bbde99946e9a41ab0
최민규 <yuming_cm4lx@naver.com>
2024-07-30 14:30:30 +0900

    comment: OpenVidu 토큰 받아오는 코드의 원본 주석 추가

--- frontend/src/routes/second/interview/PT.jsx
+++ frontend/src/routes/second/interview/PT.jsx
@@ -69,6 +69,19 @@ export default function PT() {
   configureUrls();
 
   // OpenVidu Token 가져오기
+  /**
+   * --------------------------------------------
+   * GETTING A TOKEN FROM YOUR APPLICATION SERVER
+   * --------------------------------------------
+   * The method below request the creation of a token to
+   * your application server. This prevents the need to expose
+   * your LiveKit API key and secret to the client side.
+   *
+   * In this sample code, there is no user control at all. Anybody could
+   * access your application server endpoints. In a real production
+   * environment, your application server must identify the user to allow
+   * access to the endpoints.
+   */
   const getToken = async function (roomName, participantName) {
     const response = await fetch(APPLICATION_SERVER_URL + "token", {
       method: "POST",
@@ -180,6 +193,30 @@ export default function PT() {
             </div>
             <div>
               <video ref={videoStream} autoPlay playsInline></video>
+            </div>
+            {/* OpenVidu 화상 회의 레이아웃 */}
+            <div>
+              {localTrack && (
+                <VideoComponent
+                  track={localTrack}
+                  participantIdentity={participantName}
+                  local={true}
+                />
+              )}
+              {remoteTracks.map((remoteTrack) =>
+                remoteTrack.trackPublication.kind === "video" ? (
+                  <VideoComponent
+                    key={remoteTrack.trackPublication.trackSid}
+                    track={remoteTrack.trackPublication.videoTrack}
+                    participantIdentity={remoteTrack.participantIdentity}
+                  />
+                ) : (
+                  <AudioComponent
+                    key={remoteTrack.trackPublication.trackSid}
+                    track={remoteTrack.trackPublication.audioTrack}
+                  />
+                )
+              )}
             </div>
           </div>
         </div>
