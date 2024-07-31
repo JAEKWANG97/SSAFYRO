363a4b46a64df602cdb993d4b848f62681b96d48
최민규 <yuming_cm4lx@naver.com>
2024-07-31 14:50:38 +0900

    feat: 표정 인식 AI 코드 이식

--- frontend/src/routes/second/interview/components/VideoComponent.jsx
+++ frontend/src/routes/second/interview/components/VideoComponent.jsx
@@ -2,6 +2,8 @@ import { useEffect, useRef } from "react";
 import { LocalVideoTrack, RemoteVideoTrack } from "livekit-client";
 // VideoComponent Style Sheet
 import "./VideoComponent.css";
+// face-api.js function import
+import { loadFaceAPIModels, handleVideoPlay } from "./VideoFaceApi";
 
 export default function VideoComponent({
   track,
@@ -9,10 +11,15 @@ export default function VideoComponent({
   local = false,
 }) {
   const videoElement = useRef(null);
+  const canvasRef = useRef();
 
   useEffect(() => {
     if (videoElement.current) {
       track.attach(videoElement.current);
+    }
+
+    if (local) {
+      loadFaceAPIModels();
     }
 
     return () => {
@@ -23,7 +30,20 @@ export default function VideoComponent({
   return (
     <>
       <div id={"camera-" + participantIdentity} className="rounded bg-gray-300">
-        <video ref={videoElement} id={track.sid} className="rounded"></video>
+        <video
+          ref={videoElement}
+          id={track.sid}
+          className="rounded"
+          width={"200px"}
+          height={"200px"}
+          onCanPlayThrough={
+            local ? () => handleVideoPlay(videoElement, canvasRef) : null
+          }
+        ></video>
+        <div
+          ref={canvasRef}
+          style={{ position: "absolute", top: 0, left: 0 }}
+        />
         <p className="text-center text-gray-600 py-4">
           {participantIdentity + (local ? " (You)" : "")}
         </p>
