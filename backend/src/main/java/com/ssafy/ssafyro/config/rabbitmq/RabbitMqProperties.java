f4c266c6aebf7b99a3d79419eb2774d77fd894be
박지훈 <pjh2996@naver.com>
2024-08-01 01:58:29 +0900

    style: class 시작과 끝에 줄바꿈 삽입

--- backend/src/main/java/com/ssafy/ssafyro/config/rabbitmq/RabbitMqProperties.java
+++ backend/src/main/java/com/ssafy/ssafyro/config/rabbitmq/RabbitMqProperties.java
@@ -1,4 +1,4 @@
-package com.ssafy.ssafyro.config;
+package com.ssafy.ssafyro.config.rabbitmq;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,8 +8,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @AllArgsConstructor
 @ConfigurationProperties("spring.rabbitmq")
 public class RabbitMqProperties {
+
     private String host;
     private int port;
     private String username;
     private String password;
+    
 }
