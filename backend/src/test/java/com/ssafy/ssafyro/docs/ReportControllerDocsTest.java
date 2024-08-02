139ae67daa9987ccb6e3371400fe1e866dbe98ee
박지훈 <pjh2996@naver.com>
2024-08-02 16:13:47 +0900

    test: 면접 레포트 상세 조회 API rest docs 작성

--- backend/src/test/java/com/ssafy/ssafyro/docs/ReportControllerDocsTest.java
+++ backend/src/test/java/com/ssafy/ssafyro/docs/ReportControllerDocsTest.java
@@ -1,5 +1,8 @@
 package com.ssafy.ssafyro.docs;
 
+import static com.ssafy.ssafyro.api.service.report.Expression.ANGRY;
+import static com.ssafy.ssafyro.api.service.report.Expression.HAPPY;
+import static com.ssafy.ssafyro.api.service.report.Expression.NEUTRAL;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -10,18 +13,24 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.ssafy.ssafyro.api.controller.report.ReportController;
+import com.ssafy.ssafyro.api.service.report.Expression;
 import com.ssafy.ssafyro.api.service.report.ReportService;
 import com.ssafy.ssafyro.api.service.report.response.ReportListResponse;
+import com.ssafy.ssafyro.api.service.report.response.ReportResponse;
+import com.ssafy.ssafyro.domain.interviewresult.InterviewResult;
 import com.ssafy.ssafyro.domain.report.Report;
 import com.ssafy.ssafyro.domain.room.RoomType;
 import com.ssafy.ssafyro.domain.room.entity.Room;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -35,6 +44,7 @@ public class ReportControllerDocsTest extends RestDocsSupport {
 
     private final Report report = Mockito.mock(Report.class);
     private final Room room = Mockito.mock(Room.class);
+    private final InterviewResult interviewResult = Mockito.mock(InterviewResult.class);
 
     @Override
     protected Object initController() {
@@ -96,6 +106,90 @@ public class ReportControllerDocsTest extends RestDocsSupport {
                                         .description("발음 점수"),
                                 fieldWithPath("response.reports[].createdDate").type(JsonFieldType.ARRAY)
                                         .description("면접 날짜"),
+                                fieldWithPath("error").type(JsonFieldType.NULL)
+                                        .description("에러")
+                        )
+                ));
+    }
+
+    @DisplayName("면접 레포트 상세 조회 API")
+    @Test
+    void getReport() throws Exception {
+        given(room.getId()).willReturn("roomId");
+        given(room.getTitle()).willReturn("title");
+        given(room.getType()).willReturn(RoomType.PERSONALITY);
+        given(room.getCreatedDate()).willReturn(LocalDateTime.now());
+
+        given(report.getId()).willReturn(1L);
+        given(report.getRoom()).willReturn(room);
+        given(report.getTotalScore()).willReturn(90);
+        given(report.getPronunciationScore()).willReturn(3);
+
+        given(interviewResult.getReport()).willReturn(report);
+        given(interviewResult.getQuestion()).willReturn("질문1");
+        given(interviewResult.getAnswer()).willReturn("답변");
+        given(interviewResult.getFeedback()).willReturn("피드백");
+        given(interviewResult.getPronunciationScore()).willReturn(3);
+        given(interviewResult.getAngry()).willReturn(0.01);
+        given(interviewResult.getFear()).willReturn(0.01);
+        given(interviewResult.getDisgust()).willReturn(0.01);
+        given(interviewResult.getHappy()).willReturn(0.01);
+        given(interviewResult.getNeutral()).willReturn(0.01);
+        given(interviewResult.getSad()).willReturn(0.01);
+        given(interviewResult.getSurprise()).willReturn(0.01);
+
+        Map<Expression, Double> expression = new HashMap<>();
+        expression.put(NEUTRAL, 0.9);
+        expression.put(HAPPY, 0.13);
+        expression.put(ANGRY, 0.17);
+
+        ReportResponse response = ReportResponse.of(
+                List.of(interviewResult), expression
+        );
+
+        given(reportService.getReport(any(Long.class)))
+                .willReturn(response);
+
+        mockMvc.perform(
+                        get("/api/v1/report/{id}", report.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("report-detail-list",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("id").description("report 고유 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN)
+                                        .description("성공 여부"),
+                                fieldWithPath("response").type(JsonFieldType.OBJECT)
+                                        .description("응답"),
+                                fieldWithPath("response.reportDetails").type(JsonFieldType.ARRAY)
+                                        .description("레포트 상세 정보"),
+                                fieldWithPath("response.reportDetails[].question").type(JsonFieldType.STRING)
+                                        .description("각 질문"),
+                                fieldWithPath("response.reportDetails[].answer").type(JsonFieldType.STRING)
+                                        .description("질문 별 답변"),
+                                fieldWithPath("response.reportDetails[].feedback").type(JsonFieldType.STRING)
+                                        .description("답변의 피드백"),
+                                fieldWithPath("response.reportDetails[].pronunciationScore").type(JsonFieldType.NUMBER)
+                                        .description("질문 별 발음 점수"),
+                                fieldWithPath("response.reportDetails[].expression").type(JsonFieldType.OBJECT)
+                                        .description("표정 점수 상위 3개 \n (HAPPY,\n"
+                                                + "    DISGUST,\n"
+                                                + "    SAD,\n"
+                                                + "    SURPRISE,\n"
+                                                + "    FEAR,\n"
+                                                + "    ANGRY,\n"
+                                                + "    NEUTRAL)"),
+                                fieldWithPath("response.reportDetails[].expression.ANGRY").type(JsonFieldType.NUMBER)
+                                        .description("표정 점수: 화남"),
+                                fieldWithPath("response.reportDetails[].expression.NEUTRAL").type(JsonFieldType.NUMBER)
+                                        .description("표정 점수: 중립"),
+                                fieldWithPath("response.reportDetails[].expression.HAPPY").type(JsonFieldType.NUMBER)
+                                        .description("표정 점수: 행복"),
                                 fieldWithPath("error").type(JsonFieldType.NULL)
                                         .description("에러")
                         )
