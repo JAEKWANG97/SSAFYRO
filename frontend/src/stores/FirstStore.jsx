59d232a88159f98d795f44eca64054197fc60669
고도연 <focus0007@naver.com>
2024-08-02 14:36:11 +0900

    design: essay-alert

--- frontend/src/stores/FirstStore.jsx
+++ frontend/src/stores/FirstStore.jsx
@@ -1,11 +1,13 @@
 import { create } from 'zustand';
-import Level from './../routes/first/components/Level' // Level 컴포넌트가 있는 경로를 정확히 지정해주세요
+import Level from './../routes/first/components/Level' 
 
 const useFirstStore = create((set, get) => ({
   activeTab: 'essay', // 에세이, sw적성검사 여부
   setActiveTab: (tab) => set({ activeTab: tab }),
   selected: 'major', // 전공자, 비전공자 유무
   setSelected: (select) => set({ selected: select }),
+  essayContent: '' , // 에세이 내용
+  setEssayContent: (content) => set({ essayContent: content }),
   showCorrection: false, // ai 첨삭 버튼 클릭 여부
   setShowCorrection: (value) => set({ showCorrection: value }),
   icons: [
