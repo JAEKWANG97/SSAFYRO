316ab73edcd663a6eda51866e56cb85ff63959c5
유재광 <marmong9770@gmail.com>
2024-08-03 15:28:33 +0900

    chore: InterviewRedisRepositoryTest Disabled로 변경

--- backend/src/test/java/com/ssafy/ssafyro/domain/interview/InterviewRedisRepositoryTest.java
+++ backend/src/test/java/com/ssafy/ssafyro/domain/interview/InterviewRedisRepositoryTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 
-//@Disabled
+@Disabled
 class InterviewRedisRepositoryTest extends IntegrationTestSupport {
 
     @Autowired
