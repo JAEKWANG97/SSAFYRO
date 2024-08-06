bcfe55e9d03ad2cd4cd07e2db2f8a5ccd441ab28
고도연 <focus0007@naver.com>
2024-08-07 08:59:47 +0900

    design: 페이지네이션 다크모드 삭제

--- frontend/src/routes/first/Test.jsx
+++ frontend/src/routes/first/Test.jsx
@@ -185,7 +185,7 @@ export default function Test() {
             <li>
               <button
                 onClick={() => changePage(page - 1)}
-                className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 "
               >
                 <span className="sr-only">Previous</span>
                 <svg
@@ -215,7 +215,7 @@ export default function Test() {
                       page === index + 1
                         ? "text-blue-600 border border-blue-300 bg-blue-50"
                         : "text-gray-500 bg-white border border-gray-300"
-                    } hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+                    } hover:bg-gray-100 hover:text-gray-700`}
                   >
                     {index + 1}
                   </button>
@@ -225,7 +225,7 @@ export default function Test() {
             <li>
               <button
                 onClick={() => changePage(page + 1)}
-                className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
               >
                 <span className="sr-only">Next</span>
                 <svg
