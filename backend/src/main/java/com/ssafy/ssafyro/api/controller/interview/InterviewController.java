2bed506e7d1c16909df0ded3667e4af1c655d034
김두열 <enduf768640@gmail.com>
2024-08-07 12:08:37 +0900

    refactor: 면접 질문, 대답 저장 API 요청 스펙 변경
    - 헤더에 JWT Token 추가
    - request body에 userId 삭제

--- backend/src/main/java/com/ssafy/ssafyro/api/controller/interview/InterviewController.java
+++ backend/src/main/java/com/ssafy/ssafyro/api/controller/interview/InterviewController.java
@@ -4,13 +4,16 @@ import static com.ssafy.ssafyro.api.ApiUtils.success;
 
 import com.ssafy.ssafyro.api.ApiUtils.ApiResult;
 import com.ssafy.ssafyro.api.controller.interview.request.FinishRequest;
-import com.ssafy.ssafyro.api.controller.interview.request.QnAResultRequest;
+import com.ssafy.ssafyro.api.controller.interview.request.QnAResultCreateRequest;
 import com.ssafy.ssafyro.api.controller.interview.request.StartRequest;
 import com.ssafy.ssafyro.api.service.interview.InterviewService;
 import com.ssafy.ssafyro.api.service.interview.response.ArticleResponse;
 import com.ssafy.ssafyro.api.service.interview.response.FinishResponse;
+import com.ssafy.ssafyro.api.service.interview.response.QnAResultCreateResponse;
 import com.ssafy.ssafyro.api.service.interview.response.StartResponse;
+import com.ssafy.ssafyro.security.JwtAuthentication;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,14 +44,16 @@ public class InterviewController {
     @GetMapping("/api/v1/interview/pt/{roomId}")
     public ApiResult<ArticleResponse> showArticle(@PathVariable String roomId) {
         return success(
-                interviewService.showArticle(roomId)
+                interviewService.getArticle(roomId)
         );
     }
 
     @PostMapping("/api/v1/interview/question-answer-result")
-    public ApiResult<Void> saveQnAResult(@RequestBody QnAResultRequest request) {
-        interviewService.saveQnAResult(request.toServiceRequest());
-        return success(null);
+    public ApiResult<QnAResultCreateResponse> saveQnAResult(@AuthenticationPrincipal JwtAuthentication userInfo,
+                                                            @RequestBody QnAResultCreateRequest request) {
+        return success(
+                interviewService.createQnAResult(userInfo.id(), request.toServiceRequest())
+        );
     }
 
 }
