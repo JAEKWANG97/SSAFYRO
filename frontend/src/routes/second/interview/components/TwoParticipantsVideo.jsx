1fa84e377276ffe9ea8ca29cf797b900c4b409bc
이정준 <ofekim0@naver.com>
2024-08-07 09:08:50 +0900

    design : 표정인식 위치 조정중

--- frontend/src/routes/second/interview/components/TwoParticipantsVideo.jsx
+++ frontend/src/routes/second/interview/components/TwoParticipantsVideo.jsx
@@ -13,6 +13,7 @@ export default function TwoParticipantsVideo({
   handleEndInterview,
   userList,
 }) {
+
   return (
     <>
       <div className="w-1/2 p-2 bg-gray-300 rounded-2xl mr-5 flex justify-center items-end relative">
@@ -90,26 +91,11 @@ export default function TwoParticipantsVideo({
             </svg>
           </button>
           <button className="p-3 bg-gray-700 bg-opacity-50 rounded-full w-12 h-12">
-            {/* <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="1.5em"
-                  height="1.5em"
-                  viewBox="0 0 512 512"
-                >
-                  <path
-                    fill="white"
-                    d="M256 16C123.452 16 16 123.452 16 256s107.452 240 240 240s240-107.452 240-240S388.548 16 256 16m147.078 387.078a207.253 207.253 0 1 1 44.589-66.125a207.3 207.3 0 0 1-44.589 66.125"
-                  ></path>
-                  <path
-                    fill="white"
-                    d="M152 200h40v40h-40zm168 0h40v40h-40zm18.289 107.2A83.6 83.6 0 0 1 260.3 360h-8.6a83.6 83.6 0 0 1-77.992-52.8l-1.279-3.2h-34.461L144 319.081A116 116 0 0 0 251.7 392h8.6A116 116 0 0 0 368 319.081L374.032 304h-34.464Z"
-                  ></path>
-                </svg> */}
-            <img
+            {/* <img
               src={"/emotion/" + faceExpression + ".png"} // 이모티콘 이미지
               alt="face expression"
               className="w-full h-full object-contain"
-            />
+            /> */}
           </button>
         </div>
       </div>
--- frontend/src/stores/useRoomStore.jsx
+++ frontend/src/stores/useRoomStore.jsx
@@ -1,4 +1,4 @@
-import create from 'zustand';
+import { create } from 'zustand';
 
 const useRoomStore = create((set) => ({
   userList: [],
