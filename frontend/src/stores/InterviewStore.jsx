c0bc0a0271b7b82085c6e52aac3539489ac821c3
최민규 <yuming_cm4lx@naver.com>
2024-08-08 09:42:43 +0900

    feat: 인터뷰 질문을 저장하는 InterviewStore 작성

---
+++ frontend/src/stores/InterviewStore.jsx
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+
+const useInterviewStore = create((set) => ({
+  PTQuestions: [],
+  setPTQuestions: (questions) => set({PTQuestions: questions}),
+
+  personalityQuestions: ["1분 자기소개를 해주세요.", "본인이 다른 지원자들보다 SSAFY에서 공부해야 하는 이유가 무엇인가요?"]
+}));
+
+export default useInterviewStore;
