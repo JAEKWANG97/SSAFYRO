3835390705d9b035e8ebb7a02565d8326d12fa8b
유재광 <marmong9770@gmail.com>
2024-08-08 15:26:36 +0900

    !HOTFIX: 프롬프트 변경

--- backend/src/test/java/com/ssafy/ssafyro/api/service/interview/ChatGptResponseGeneratorTest.java
+++ backend/src/test/java/com/ssafy/ssafyro/api/service/interview/ChatGptResponseGeneratorTest.java
@@ -45,8 +45,8 @@ class ChatGptResponseGeneratorTest extends IntegrationTestSupport {
     @Test
     void generateNewEssayTest() {
         // given
-        String question = "";
-        String answer = "";
+        String question = " 향후  어떤 SW 개발자로 성장하고 싶은지 SW 관련 경험을 토대로 기술하고, SSAFY에 지원하신 동기에 대해서도 작성 바랍니다.";
+        String answer = "저는 단순한 불편함을 넘어서 근본적인 문제를 발견하고 해결하는 개발자로 성장하고자 합니다. 대학교 빅데이터 분석 수업에서의 도전, 송파구 대피소 최적화 프로젝트는 이러한 철학을 실천하는데 중요한 경험이였습니다. 5명 중 4명이 미참여한 이 프로젝트에서, 저는 데이터 분석을 통해 대피소 할당 문제의 본질을 파악하고 k-means 알고리즘을 변형 후 적용하여 실질적인 해결책을 찾았습니다. 이 과정에서 저는 복잡한 문제를 깊이 분석하고, 창의적으로 해결하는 능력을 키웠습니다.";
 
         // when
         String newEssay = chatGptResponseGenerator.generateNewEssay(question, answer);
@@ -54,4 +54,6 @@ class ChatGptResponseGeneratorTest extends IntegrationTestSupport {
         // then
         System.out.println(newEssay);
     }
+
+
 }
