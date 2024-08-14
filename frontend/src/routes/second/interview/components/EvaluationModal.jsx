e6ffa75084dd7e1daf7445d546901fe0bf8878c9
최민규 <yuming_cm4lx@naver.com>
2024-08-14 17:42:29 +0900

    feat: evaluationScore 제출하는 모달 작성

--- frontend/src/routes/second/interview/components/EvaluationModal.jsx
+++ frontend/src/routes/second/interview/components/EvaluationModal.jsx
@@ -1,3 +1,27 @@
+import { useState } from "react"
+
 export default function EvaluationModal ({ targetUser, setEvaluationModal, handleEvaluation }) {
-  <></>
+  const [evaluationScore, setEvaluationScore] = useState(0);
+  
+  return (<>
+    <div>
+      {[1, 2, 3, 4, 5].map((option) => (
+        <label key={option}>
+          {option}
+          <input type="radio"
+            value={option}
+            onChange={(e) => setEvaluationScore(e.target.value)}
+           />
+        </label>
+      ))}
+    </div>
+    <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+      onClick={() => {
+        handleEvaluation(targetUser, evaluationScore);
+        setEvaluationModal(false);
+      }}
+    >
+      평가 점수 제출
+    </button>
+  </>)
 };
