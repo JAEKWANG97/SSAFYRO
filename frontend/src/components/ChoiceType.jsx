26da85278e53a8bf626505763dbe21e9a3fa94e6
JAEKWANG97 <marmong@naver.com>
2026-07-11 11:43:20 +0900

    chore: remove legacy frontend and presentation artifacts

--- frontend/src/components/ChoiceType.jsx
+++
@@ -1,8 +0,0 @@
-import Swal from "sweetalert2";
-import { useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
-import { initUserType } from './../api/profileApi';
-
-export default function ChoiceType() {
-  const nav = useNavigate();
-}
